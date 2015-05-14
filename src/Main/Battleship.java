3e4a1c3cd3010e1263f14dc71b70a78b96318b69
Tabbidabbi <winkler.bremen@hotmail.com>
2015-05-14 19:32:26 +0200

    Bugfixing User Input
    ammountOfPlayer Methode gelöscht, setX und Y Methode gelöscht.

--- src/Main/Battleship.java
+++ src/Main/Battleship.java
@@ -1,6 +1,7 @@
 package Main;
 
 import Player.Player;
+import Ships.Ship;
 import IO.IO;
 
 /**
@@ -15,9 +16,21 @@ public class Battleship {
         int shipX;//x-koodinate
         int shipY;//y-koordinate
         boolean shipO;// Ausrichtung des Schiffes (h,v)
+        Ship ships[];
+        boolean error = false;
 
         IO.println("Herzlich Willkommen zu Schiffe versenken!");
-        int amountOfPlayer = setAmountOfPlayers();//Spieleranzahl 
+        do {
+            IO.print("Geben Sie die Anzahl der Spieler ein (2-6): ");
+            amountOfPlayer = IO.readInt();
+            if (amountOfPlayer < 2 || amountOfPlayer > 6) {
+                error = true;
+                IO.println("Fehler");
+            } else {
+                error = false;
+            }
+        } while (error);
+        //int amountOfPlayer = setAmountOfPlayers();//Spieleranzahl 
 
         //Spieler-Array erstellen
         Player[] player = new Player[amountOfPlayer];//Spieleranzahl holen
@@ -28,126 +41,114 @@ public class Battleship {
 
             IO.print("Spieler " + player[i].getName() + " , " + "Sie können nun " + player[i].getShips().length + " Schiffe setzen: " + "\n"
                     + "1 Zerstörer, 1 Fregatte, 2 Corvetten und 2 U-Boote." + "\n");
-            for (int s = 0; s < player[i].getShips().length;) {
-                IO.print("Bitte geben Sie die X-Koordinate für " + player[i].getShips()[s].getName() + " ein:");
+            ships = player[i].getShips();
+            for (int s = 0; s < ships.length;) {
+                IO.print("Bitte geben Sie die X-Koordinate für " + ships[s].getName() + " ein:");
                 shipX = IO.readInt();
-                IO.print("Bitte geben Sie die Y-Koordinate für " + player[i].getShips()[s].getName() + " ein:");
+                IO.print("Bitte geben Sie die Y-Koordinate für " + ships[s].getName() + " ein:");
                 shipY = IO.readInt();
-                IO.print("Bitte geben Sie die Ausrichtung des Schiffes an (h = horizontal, v = vertical: ");
+                //IO.print("Bitte geben Sie die Ausrichtung des Schiffes an (h = horizontal, v = vertical: ");
                 shipO = setOrientation();
-                if (!player[i].getShips()[s].placeShip(shipX, shipY, shipO, player[i].getField())) {
+                if (!ships[s].placeShip(shipX, shipY, shipO, player[i].getField())) {
                     IO.println("Das Schiff konnte nicht gesetzt werden. Bitte erneut versuchen.");
                 } else {
                     s++;
                 }
             }
         }
-    
-        //Runde beginnt
-    //solange es mehr als einen spieler gibt
-    while (player.length<
 
-    
-        1) {
+        //Runde beginnt
+        //solange es mehr als einen spieler gibt
+        while (player.length
+                < 1) {
             for (int pl = 0; pl < player.length; pl++) {
 
-            //Spieler, die verloren haben, kommen nicht mehr an die Reihe
-            if (player[pl].getIsLost() == false) {
+                //Spieler, die verloren haben, kommen nicht mehr an die Reihe
+                if (player[pl].getIsLost() == false) {
 
-                //Setzt die Nachladezeit aller Schiffe in jeder Runde um 1 runter
-                for (int sh = 0; sh < player[pl].getShips().length; sh++) {
-                    player[pl].getShips()[sh].setDownReloadTime();
-                }
-
-                //Runde des Spielers pla
-                for (int pla = 0; pla < player.length; pla++) {
-                    IO.println("Spieler " + player[pla].getNumber() + " " + player[pla].getName() + " ist am Zug!");
-
-                    //Abfrage, welcher Spieler angegriffen werden soll
-                    IO.println("Welchen Spieler m�chtest du angreifen?");
-
-                    //Gibt die Liste aller Spieler aus, die angegriffen werden k�nnen
-                    printListOfOpponents(player, pla);
-
-                    IO.print("Gebe die Nummer des Gegners ein: ");
-
-                    //Einlesen des SPielers, den man angreifen will
-                    int opponent = IO.readInt();
-                    //Wert f�r Array, denn Array f�ngt bei 0 an zu z�hlen
-                    opponent = opponent - 1;
-
-                    //Gibt das Spielfeld des Gegners aus
-                    player[opponent].getField().printOpponentField();
-
-                    //Schiff
-                    IO.println("Mit welchem Schiff willst du schie�en?");
-                    for (int shi = 0; shi < player[pla].getShips().length; shi++) {
-                        IO.println("Nummer: " + player[pla].getShips()[shi].getNumber() + " Typ: " + player[pla].getShips()[shi].getName());
+                    //Setzt die Nachladezeit aller Schiffe in jeder Runde um 1 runter
+                    for (int sh = 0; sh < player[pl].getShips().length; sh++) {
+                        player[pl].getShips()[sh].setDownReloadTime();
                     }
 
-                    //Eingabe, welches Schiff schie�en soll
-                    IO.println("Gebe die Nummer des Schiffs ein: ");
+                    //Runde des Spielers pla
+                    for (int pla = 0; pla < player.length; pla++) {
+                        IO.println("Spieler " + player[pla].getNumber() + " " + player[pla].getName() + " ist am Zug!");
 
-                    //Einlesen des Schiffs
-                    int ship = IO.readInt();
-                    ship = ship - 1;
+                        //Abfrage, welcher Spieler angegriffen werden soll
+                        IO.println("Welchen Spieler m�chtest du angreifen?");
 
-                    //Reichweite des Schusses, um diese der Methode setShot zu �bergeben
-                    int shootRange = player[pl].getShips()[ship].getShootRange();
+                        //Gibt die Liste aller Spieler aus, die angegriffen werden k�nnen
+                        printListOfOpponents(player, pla);
 
-                    //Abfrage
-                    IO.println("Wo soll das Schiff hinschie�en?");
-                    //Einlesen X-Koordinate
-                    IO.print("X = ");
-                    int x = IO.readInt();
-                    //Einlesen Y-Koordinate
-                    IO.print("Y = ");
-                    int y = IO.readInt();
+                        IO.print("Gebe die Nummer des Gegners ein: ");
+
+                        //Einlesen des SPielers, den man angreifen will
+                        int opponent = IO.readInt();
+                        //Wert f�r Array, denn Array f�ngt bei 0 an zu z�hlen
+                        opponent = opponent - 1;
+
+                        //Gibt das Spielfeld des Gegners aus
+                        player[opponent].getField().printOpponentField();
+
+                        //Schiff
+                        IO.println("Mit welchem Schiff willst du schie�en?");
+                        for (int shi = 0; shi < player[pla].getShips().length; shi++) {
+                            IO.println("Nummer: " + player[pla].getShips()[shi].getNumber() + " Typ: " + player[pla].getShips()[shi].getName());
+                        }
+
+                        //Eingabe, welches Schiff schie�en soll
+                        IO.println("Gebe die Nummer des Schiffs ein: ");
+
+                        //Einlesen des Schiffs
+                        int ship = IO.readInt();
+                        ship = ship - 1;
+
+                        //Reichweite des Schusses, um diese der Methode setShot zu �bergeben
+                        int shootRange = player[pl].getShips()[ship].getShootRange();
+
+                        //Abfrage
+                        IO.println("Wo soll das Schiff hinschie�en?");
+                        //Einlesen X-Koordinate
+                        IO.print("X = ");
+                        int x = IO.readInt();
+                        //Einlesen Y-Koordinate
+                        IO.print("Y = ");
+                        int y = IO.readInt();
 
                         //Schie�en
-                    //Felder des gegnerischen Spielers werden auf abgeschossen gesetzt
-                    player[pla].getField().setShot(x, y, shootRange);
-                    player[pla].getOpponentField().setShot(x, y, shootRange);
-                    player[pla].getOpponentField().printOpponentField();
+                        //Felder des gegnerischen Spielers werden auf abgeschossen gesetzt
+                        player[pla].getField().setShot(x, y, shootRange);
+                        player[pla].getOpponentField().setShot(x, y, shootRange);
+                        player[pla].getOpponentField().printOpponentField();
 
-                    if (player[pla].getIsLost() == true) {
-                        //Spieler player[pla] aus dem Spieler-Array nehmen
+                        if (player[pla].getIsLost() == true) {
+                            //Spieler player[pla] aus dem Spieler-Array nehmen
+                        }
                     }
                 }
-            }
-            //Setzt pl auf 0, damit die Runde vorn beginnt
-            if (pl == player.length) {
-                pl = 0;
-            }
+                //Setzt pl auf 0, damit die Runde vorn beginnt
+                if (pl == player.length) {
+                    pl = 0;
+                }
 
+            }
         }
     }
-}
 
-public static int setAmountOfPlayers() {
-        IO.print("Geben Sie die Anzahl der Spieler ein (2-6): ");
-        int amountOfPlayer = IO.readInt();
-        if (amountOfPlayer <= 1 || amountOfPlayer > 6) {
-            IO.println("Es müssen mindestens 2 und maximal 6 Spieler das Spiel spielen!");
-            setAmountOfPlayers();
-        }
-        return amountOfPlayer;
-    }
 
-    public static int setXCoordinate() {
-        IO.print("Geben Sie die x-Koordinate zum Setzen des Schiffs ein: ");
-        int x = IO.readInt();
-        return x;
-    }
+//    public static int setCoordinate(int max) {
+//        int c = IO.readInt();
+//        if (c < 0 || c > max) {
+//            return -1;
+//        }
+//        return c;
+//    }
 
-    public static int setYCoordinate() {
-        IO.print("Geben Sie die y-Koordinate zum Setzen des Schiffs ein: ");
-        int y = IO.readInt();
-        return y;
-    }
 
     private static boolean setOrientation() {
         boolean orientation = false;
+        IO.print("Bitte geben Sie die Ausrichtung des Schiffes an (h = horizontal, v = vertical: ");
         String o = IO.readString();
         if (o.equals("h")) {
             orientation = true;
