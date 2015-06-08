3df5c9defee1eeae7aced682d115a21bd611ebac
dprigge <dprigge@stud.hs-bremen.de>
2015-06-08 14:34:29 +0200

    Künstliche Intelligenz
    AiPlayer.class

---
+++ src/Player/AiPlayer.java
@@ -0,0 +1,12 @@
+package Player;
+
+public class AiPlayer extends Player{
+
+	private String lastShot;
+
+	public AiPlayer(int number, String name) {
+		super(number, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
